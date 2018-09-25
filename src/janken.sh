61a281943b4376bea944f42e05cb6e84b5cd5a2b
MEKW0000 <42951235+MEKW0000@users.noreply.github.com>
2018-09-25 11:46:19 +0900

    janken.sh

--- src/janken.sh
+++ src/janken.sh
@@ -6,9 +6,11 @@ read -p "じゃんけん(g/c/p):" pd
 #playerの出し手を数字に変換
 p=0;
 case $pd in
-   g) p='グー';;
-   c) p='チョキ';;
-   p) p='パー';;
+   'g') p='グー';;
+   'c') p='チョキ';;
+   'p') p='パー';;
 esac
 
-echo '$p'
+echo $p
+
+#CPUの出し手
