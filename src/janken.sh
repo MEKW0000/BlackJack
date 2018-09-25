d0d583295aa0b5f356828ca49bad3201e5114eff
MEKW0000 <42951235+MEKW0000@users.noreply.github.com>
2018-09-25 13:31:48 +0900

    janken.sh

--- src/janken.sh
+++ src/janken.sh
@@ -4,15 +4,31 @@
 read -p "じゃんけん(g/c/p):" pd
 
 #playerの出し手を数字に変換
-p=0;
+#0がグー、1がチョキ、2がパー
 case $pd in
-   'g') p='グー';;
-   'c') p='チョキ';;
-   'p') p='パー';;
+   'g') pd='グー';;
+   'c') pd='チョキ';;
+   'p') pd='パー';;
 esac
 
 
 #CPUの出し手
-cd=$((RANDOM%3))
+#0がグー、1がチョキ、2がパー
+cd=$((RANDOM%3));
+case $cd in
+  0 ) cd='グー';;
+  1 ) cd='チョキ';;
+  2 ) cd='パー';;
+esac
 
-echo $cd
+#じゃんけん処理
+#playerがグーの時
+if [ $pd = $cd ];then
+  echo 'あいこです。'
+elif [ $pd = 'グー' ] && [ $cd = 'チョキ'];then
+  echo 'playerの勝ちです。'
+elif [ $pd = 'チョキ'] && [ $cd = 'パー'];then
+  echo 'playerの勝ちです。'
+elif [ $pd = 'パー'] && [ $cd = 'グー'];then
+  echo 'playerの勝ちです。'
+  fi
